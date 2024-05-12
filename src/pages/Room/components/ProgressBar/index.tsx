c4d4dfbeb15d5503eb40414bfc7843bbd0f44cea
Nathan Chacon <chacondorodo@gmail.com>
2024-05-12 16:10:20 -0300

    adjust style

--- src/pages/Room/components/ProgressBar/index.tsx
+++ src/pages/Room/components/ProgressBar/index.tsx
@@ -1,7 +1,7 @@
 const ProgressBar = ({percentage}: {percentage: number}) => {
   return (
-    <div style={{ width: '100%', backgroundColor: '#f0f0f0', height: '5px', borderRadius: '5px', maxWidth: "755px" }}>
-        <div style={{ width: `${percentage}%`, backgroundColor: '#4CAF50', height: '100%', borderRadius: '5px', transition: 'width 0.1s ease' }}></div>
+    <div style={{ width: '100%', backgroundColor: '#f0f0f0', height: '5px', maxWidth: "755px" }}>
+        <div style={{ width: `${percentage}%`, backgroundColor: 'rgb(236 0 255)', height: '100%', transition: 'width 0.1s ease' }}></div>
     </div>
   );
 };
